8ee344794fdb5ad420e625510ae993a3eeb9fbba
NelsonSu <33085066+nelson40514@users.noreply.github.com>
2023-12-17 22:14:21 +0000

    Update README.md with Flask implementation for Linebot on Vercel

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
-# vercel-linebot
-A Vercel Linebot Echo Server
+# vercel_linebot_flask
+使用Python Flask框架建立Linebot機器人並部署在Vercel上
+
+## 開發說明
+在將專案clone後，複製`.env.example`為`.env`並在`=`右邊貼上ACCESS_TOKEN及CHANNEL_SECRET
+執行 `pip install -r requirements.txt` 安裝套件
+專案目錄下在終端機輸入`python api/index.py`可以執行程式
---
+++ main.py
@@ -0,0 +1,48 @@
+from flask import Flask, request, abort
+from linebot import LineBotApi, WebhookHandler
+from linebot.exceptions import InvalidSignatureError
+from linebot.models import MessageEvent, TextMessage, TextSendMessage
+
+import os
+import csv
+from dotenv import load_dotenv
+
+# 載入.env檔案中的變數
+load_dotenv()
+
+
+line_bot_api = LineBotApi(os.getenv("LINE_CHANNEL_ACCESS_TOKEN"))
+line_handler = WebhookHandler(os.getenv("LINE_CHANNEL_SECRET"))
+
+app = Flask(__name__)
+
+# domain root
+@app.route('/')
+def home():
+    return 'Hello, World!'
+
+@app.route("/webhook", methods=['POST'])
+def callback():
+    # get X-Line-Signature header value
+    signature = request.headers['X-Line-Signature']
+    # get request body as text
+    body = request.get_data(as_text=True)
+    app.logger.info("Request body: " + body)
+    # handle webhook body
+    try:
+        line_handler.handle(body, signature)
+    except InvalidSignatureError:
+        abort(400)
+    return 'OK'
+
+
+@line_handler.add(MessageEvent, message=TextMessage)
+def handle_message(event):
+    line_bot_api.reply_message(
+        event.reply_token,
+        TextSendMessage(text=event.message.text)
+    )
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0")
